d74792ff5090aeeed0313e6ff48fa734e3eb859b
hzs <1461239545@qq.com>
2024-10-25 13:01:14 +0800

    lab1

---
+++ lab1/minnow/src/reassembler.hh
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "byte_stream.hh"
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+class Reassembler
+{
+public:
+  // Construct Reassembler to write into given ByteStream.
+  explicit Reassembler( ByteStream&& output )
+    : output_( std::move( output ) ), sum( 0 ), assemble(), vv(), is_over( false )
+  {}
+
+  /*
+   * Insert a new substring to be reassembled into a ByteStream.
+   *   `first_index`: the index of the first byte of the substring
+   *   `data`: the substring itself
+   *   `is_last_substring`: this substring represents the end of the stream
+   *   `output`: a mutable reference to the Writer
+   *
+   * The Reassembler's job is to reassemble the indexed substrings (possibly out-of-order
+   * and possibly overlapping) back into the original ByteStream. As soon as the Reassembler
+   * learns the next byte in the stream, it should write it to the output.
+   *
+   * If the Reassembler learns about bytes that fit within the stream's available capacity
+   * but can't yet be written (because earlier bytes remain unknown), it should store them
+   * internally until the gaps are filled in.
+   *
+   * The Reassembler should discard any bytes that lie beyond the stream's available capacity
+   * (i.e., bytes that couldn't be written even if earlier gaps get filled in).
+   *
+   * The Reassembler should close the stream after writing the last byte.
+   */
+  void insert( uint64_t first_index, std::string data, bool is_last_substring );
+
+  // How many bytes are stored in the Reassembler itself?
+  uint64_t bytes_pending() const;
+
+  // Access output stream reader
+  Reader& reader() { return output_.reader(); }
+  const Reader& reader() const { return output_.reader(); }
+
+  // Access output stream writer, but const-only (can't write from outside)
+  const Writer& writer() const { return output_.writer(); }
+
+private:
+  ByteStream output_; // the Reassembler writes to this ByteStream
+  uint64_t sum;
+  multimap<uint64_t, string> assemble;
+  vector<char> vv;
+  bool is_over;
+};
